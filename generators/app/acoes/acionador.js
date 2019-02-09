d2f52957469ab8aa02c54b0dc7b52d9d6df45721
Josemberg Ferreira <josembergff@gmail.com>
2019-02-09 13:07:45 -0200

    ajustes de repositorios e controle

--- generators/app/acoes/acionador.js
+++ generators/app/acoes/acionador.js
@@ -1,69 +1,84 @@
 'use strict';
-const rename = require("gulp-rename");
+const rename = require('gulp-rename');
 let destino = '';
-// destino = './generators/temp';
+//destino = './../generators/teste';
 
 exports.renomearEntidade = (atual, data) => {
-    atual.registerTransformStream(rename(function (path) {
-        path.basename = path.basename.replace(/(<%=nomeEntidade%>)/g, data.nomeEntidade);
-        path.dirname = path.dirname.replace(/(<%=nomeEntidade%>)/g, data.nomeEntidade);
-    }));
+  atual.registerTransformStream(
+    rename(function(path) {
+      path.basename = path.basename.replace(
+        /(<%=nomeEntidade%>)/g,
+        data.nomeEntidade
+      );
+      path.dirname = path.dirname.replace(
+        /(<%=nomeEntidade%>)/g,
+        data.nomeEntidade
+      );
+    })
+  );
 };
 
 exports.novoProjetoApiNodeMongoose = (atual, data) => {
-    atual.fs.copyTpl(
-        atual.templatePath('./padraoApiMongoose/estrutura'),
-        atual.destinationPath(destino), {
-            nomeProjeto: data.nomeProjeto,
-            nomeExternoProjeto: data.nomeExternoProjeto,
-            descricaoProjeto: data.descricaoProjeto,
-            chaveMongo: data.chaveMongo,
-            chaveSendgrid: data.chaveSendgrid
-        }
-    );
-    atual.log('Obs.: Após criado o projeto novo, confirme as configurações de acesso ao Mongo e SendGrid em "src/config/parameters.js". Execute o "npm install" atualizar os módulos.');
+  atual.fs.copyTpl(
+    atual.templatePath('./padraoApiMongoose/estrutura'),
+    atual.destinationPath(destino),
+    {
+      nomeProjeto: data.nomeProjeto,
+      nomeExternoProjeto: data.nomeExternoProjeto,
+      descricaoProjeto: data.descricaoProjeto,
+      chaveMongo: data.chaveMongo,
+      chaveSendgrid: data.chaveSendgrid
+    }
+  );
+  atual.log(
+    'Obs.: Após criado o projeto novo, confirme as configurações de acesso ao Mongo e SendGrid em "src/config/parameters.js". Execute o "npm install" atualizar os módulos.'
+  );
 };
 
 exports.novoRepositorioApiNodeMongoose = (atual, data) => {
-    atual.fs.copyTpl(
-        atual.templatePath('./padraoApiMongoose/repositorio'),
-        atual.destinationPath(destino), {
-            nomeEntidade: data.nomeEntidade,
-            nomeExternoEntidade: data.nomeExternoEntidade,
-            nomeExternoEntidadePlural: data.nomeExternoEntidadePlural
-        }
-    );
+  atual.fs.copyTpl(
+    atual.templatePath('./padraoApiMongoose/repositorio'),
+    atual.destinationPath(destino),
+    {
+      nomeEntidade: data.nomeEntidade,
+      nomeExternoEntidade: data.nomeExternoEntidade,
+      nomeExternoEntidadePlural: data.nomeExternoEntidadePlural
+    }
+  );
 };
 
 exports.novoModeloApiNodeMongoose = (atual, data) => {
-    atual.fs.copyTpl(
-        atual.templatePath('./padraoApiMongoose/modelo'),
-        atual.destinationPath(destino), {
-            nomeEntidade: data.nomeEntidade,
-            nomeExternoEntidade: data.nomeExternoEntidade,
-            nomeExternoEntidadePlural: data.nomeExternoEntidadePlural
-        }
-    );
+  atual.fs.copyTpl(
+    atual.templatePath('./padraoApiMongoose/modelo'),
+    atual.destinationPath(destino),
+    {
+      nomeEntidade: data.nomeEntidade,
+      nomeExternoEntidade: data.nomeExternoEntidade,
+      nomeExternoEntidadePlural: data.nomeExternoEntidadePlural
+    }
+  );
 };
 
 exports.novoControleApiNodeMongoose = (atual, data) => {
-    atual.fs.copyTpl(
-        atual.templatePath('./padraoApiMongoose/controle'),
-        atual.destinationPath(destino), {
-            nomeEntidade: data.nomeEntidade,
-            nomeExternoEntidade: data.nomeExternoEntidade,
-            nomeExternoEntidadePlural: data.nomeExternoEntidadePlural
-        }
-    );
+  atual.fs.copyTpl(
+    atual.templatePath('./padraoApiMongoose/controle'),
+    atual.destinationPath(destino),
+    {
+      nomeEntidade: data.nomeEntidade,
+      nomeExternoEntidade: data.nomeExternoEntidade,
+      nomeExternoEntidadePlural: data.nomeExternoEntidadePlural
+    }
+  );
 };
 
 exports.novoRotaApiNodeMongoose = (atual, data) => {
-    atual.fs.copyTpl(
-        atual.templatePath('./padraoApiMongoose/rota'),
-        atual.destinationPath(destino), {
-            nomeEntidade: data.nomeEntidade,
-            nomeExternoEntidade: data.nomeExternoEntidade,
-            nomeExternoEntidadePlural: data.nomeExternoEntidadePlural
-        }
-    );
+  atual.fs.copyTpl(
+    atual.templatePath('./padraoApiMongoose/rota'),
+    atual.destinationPath(destino),
+    {
+      nomeEntidade: data.nomeEntidade,
+      nomeExternoEntidade: data.nomeExternoEntidade,
+      nomeExternoEntidadePlural: data.nomeExternoEntidadePlural
+    }
+  );
 };
